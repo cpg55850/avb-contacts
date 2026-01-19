bf5c577bf48646b93925a1fb371257e532ea96cf
Charlie Graham <cpg55850@ucmo.edu>
2026-01-18 23:16:48 -0600

    server side contacts on initial render

--- app/routes/frontend.py
+++ app/routes/frontend.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template
+from sqlalchemy import func
 from app.models import Contact
 
 frontend = Blueprint("frontend", __name__)
 
 @frontend.route("/")
 def index():
-    contacts = Contact.query.order_by(Contact.name.asc()).all()
+    contacts = Contact.query.order_by(func.lower(Contact.name).asc()).all()
     return render_template("index.html", test="Hello, World!", contacts=contacts)
